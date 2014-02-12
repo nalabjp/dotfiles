c7cd3c012613654ccfe47760c9e2dd6d066092de
nalabjp <nalabjp@gmail.com>
2014-02-12 15:44:41 +0900

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd ~/.dotfiles
+echo "cd $PWD"
+
+echo ''
+echo 'start ./before_brew_bundle.sh'
+./before_brew_bundle.sh
+echo 'finish ./before_brew_bundle.sh'
+
+echo ''
+echo 'start brew bundle'
+brew bundle
+echo 'finish brew bundle'
+
+echo ''
+echo 'start after_brew_bundle.sh'
+./after_brew_bundle.sh
+echo 'finish after_brew_bundle.sh'
+
+echo ''
+echo 'start setup.sh'
+./setup.sh
+echo 'finish setup.sh'
