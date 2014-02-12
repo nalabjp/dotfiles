3201213b3b16d8995f52cd53bf598f60e7c70f19
nalabjp <nalabjp@gmail.com>
2014-02-12 12:04:37 +0900

    Create rbenv_setup.sh

---
+++ rbenv_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# ruby install
+rbenv install 1.9.3-p484
+rbenv install 2.0.0-p353
+rbenv install 2.1.0
+rbenv global 2.1.0
+
+# gem install to global
+gem install pry pry-byebug pry-doc pry-stack_explorer awesome_print tapp
+
+# gem install to system for weechat/ruby/notification_center.rb
+rbenv shell system
+gem install ruby_gntp weechat terminal-notifier
+rbenv shell --unset
