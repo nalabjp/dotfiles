77e5235d69a96a94e3a4b0e6fd7ad96385a4a0de
nalabjp <nalabjp@gmail.com>
2014-02-26 16:57:55 +0900

    Refactoring

--- rbenv_setup.sh
+++ rbenv_setup.sh
@@ -1,35 +1,15 @@
 #!/bin/bash
 
+source ./rbenv_functions.sh
+
 RUBIES=(1.9.3-p484 2.0.0-p353 2.1.0 2.1.1)
-GEMLIST_TO_SYSTEM=(ruby_gntp weechat terminal-notifier tmuxinator)
-GEMLIST_TO_VERSION=(bundler pry pry-byebug pry-doc pry-rescue pry-stack_explorer awesome_print tapp)
-
-install_gem_if_not_installed() {
-    local gem=$1
-    echo [$gem]
-    if [ `gem list $gem -i` = 'false' ]; then
-        echo '  now installing...'
-        gem install $gem
-        rbenv rehash
-    else
-        echo '  already installed'
-    fi
-
-}
-
-install_gem_list_if_not_installed() {
-    local gems_list=$@
-    for gem in ${gems_list[@]}
-    do
-        install_gem_if_not_installed $gem
-    done
-}
+GEMLIST_TO_VERSION=(bundler pry pry-byebug pry-doc pry-rescue pry-stack_explorer awesome_print tapp rubocop)
 
 # gem install to system
-echo ''
-echo '## ruby system ##'
-rbenv global system
-install_gem_list_if_not_installed ${GEMLIST_TO_SYSTEM[*]}
+#echo ''
+#echo '## ruby system ##'
+#rbenv global system
+#install_gem_list_if_not_installed ${GEMLIST_TO_SYSTEM[*]}
 
 # ruby install & gem install to global
 for version in ${RUBIES[@]}
---
+++ rbenv_system_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# require system gem command path
+GEM_CMD='sudo /usr/bin/gem'
+
+source ./rbenv_functions.sh
+
+GEMLIST_TO_SYSTEM=(ruby_gntp weechat terminal-notifier tmuxinator)
+
+# gem install to system
+echo ''
+echo '## ruby system ##'
+install_gem_list_if_not_installed ${GEMLIST_TO_SYSTEM[*]}
