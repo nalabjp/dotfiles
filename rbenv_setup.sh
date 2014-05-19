d31691441100250a17570a0d00303680cbaaa89d
nalabjp <nalabjp@gmail.com>
2014-05-19 18:01:04 +0900

    Removed unused code

--- rbenv_setup.sh
+++ rbenv_setup.sh
@@ -5,12 +5,6 @@ source ./rbenv_functions.sh
 RUBIES=(1.9.3-p484 2.0.0-p353 2.1.0 2.1.1)
 GEMLIST_TO_VERSION=(bundler pry pry-byebug pry-doc pry-rescue pry-stack_explorer awesome_print tapp rubocop)
 
-# gem install to system
-#echo ''
-#echo '## ruby system ##'
-#rbenv global system
-#install_gem_list_if_not_installed ${GEMLIST_TO_SYSTEM[*]}
-
 # ruby install & gem install to global
 for version in ${RUBIES[@]}
 do
