081e523ff0900b10abd9f344fa8156b660900eef
nalabjp <nalabjp@gmail.com>
2015-12-21 08:26:42 +0900

    Update karabiner settings

--- src/karabiner-import.sh
+++ src/karabiner-import.sh
@@ -2,7 +2,7 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set remap.left_option_to_option_forward_delete 1
+$cli set remap.doublepresscommandQ 1
 /bin/echo -n .
 $cli set repeat.wait 10
 /bin/echo -n .
@@ -10,10 +10,6 @@ $cli set repeat.initial_wait 200
 /bin/echo -n .
 $cli set remap.shift_r_to_shift_delete 1
 /bin/echo -n .
-$cli set remap.doublepresscommandQ 1
-/bin/echo -n .
 $cli set private.app_slack_swap_enter_with_command_r 1
-/bin/echo -n .
-$cli set remap.command_r_delete_to_forward_delete 1
 /bin/echo -n .
 /bin/echo
