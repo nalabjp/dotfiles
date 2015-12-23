2812c9ea7f71aa8ec87fa9f1d8ffd48a1a5d324a
nalabjp <nalabjp@gmail.com>
2015-12-23 14:12:46 +0900

    Remove shift_r_to_shift_delete

--- src/karabiner-import.sh
+++ src/karabiner-import.sh
@@ -2,13 +2,11 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set remap.doublepresscommandQ 1
-/bin/echo -n .
 $cli set repeat.wait 10
 /bin/echo -n .
 $cli set repeat.initial_wait 200
 /bin/echo -n .
-$cli set remap.shift_r_to_shift_delete 1
+$cli set remap.doublepresscommandQ 1
 /bin/echo -n .
 $cli set remap.commandR2commandR_delete_keyrepeat 1
 /bin/echo -n .
