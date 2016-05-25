a5bcdac6e410d3e3c75c2ac6dbfc2829906145cf
nalabjp <nalabjp@gmail.com>
2016-05-25 12:29:15 +0900

    Add some config for karabiner

--- src/karabiner-import.sh
+++ src/karabiner-import.sh
@@ -2,12 +2,18 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set remap.doublepresscommandQ 1
+/bin/echo -n .
 $cli set repeat.wait 20
 /bin/echo -n .
 $cli set repeat.initial_wait 200
 /bin/echo -n .
-$cli set remap.doublepresscommandQ 1
+$cli set remap.commandR2enter 1
 /bin/echo -n .
-$cli set remap.commandR2commandR_delete_keyrepeat 1
+$cli set remap.jis_kana2return 1
+/bin/echo -n .
+$cli set remap.jis_eisuu2delete 1
+/bin/echo -n .
+$cli set remap.jis_commandSpace2toggle_kana_eisuu 1
 /bin/echo -n .
 /bin/echo
