751a3003cf1d5a194a035a3f26e13ea7fed2ca9c
nalabjp <nalabjp@gmail.com>
2016-05-26 12:30:33 +0900

    Remove alias of `Command_R to Enter`

--- src/karabiner-import.sh
+++ src/karabiner-import.sh
@@ -8,8 +8,6 @@ $cli set repeat.wait 20
 /bin/echo -n .
 $cli set repeat.initial_wait 200
 /bin/echo -n .
-$cli set remap.commandR2enter 1
-/bin/echo -n .
 $cli set remap.jis_kana2return 1
 /bin/echo -n .
 $cli set remap.jis_eisuu2delete 1
