4fe5b1ac11df5d8eab15406cb68131b6a96bf553
nalabjp <nalabjp@gmail.com>
2016-06-01 22:30:50 +0900

    Update config of karabiner

--- src/karabiner-import.sh
+++ src/karabiner-import.sh
@@ -2,16 +2,10 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set repeat.wait 15
+/bin/echo -n .
+$cli set repeat.initial_wait 250
+/bin/echo -n .
 $cli set remap.doublepresscommandQ 1
-/bin/echo -n .
-$cli set repeat.wait 20
-/bin/echo -n .
-$cli set repeat.initial_wait 200
-/bin/echo -n .
-$cli set remap.jis_kana2return 1
-/bin/echo -n .
-$cli set remap.jis_unify_kana_to_eisuu 1
-/bin/echo -n .
-$cli set remap.commandR2commandR_delete_keyrepeat 1
 /bin/echo -n .
 /bin/echo
