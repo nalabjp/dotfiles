28fca3e1264b4138e9dbe903c3438b3ce0e6b84c
nalabjp <nalabjp@gmail.com>
2016-06-05 12:27:00 +0900

    Update config

--- src/karabiner-import.sh
+++ src/karabiner-import.sh
@@ -2,10 +2,24 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set repeat.wait 15
-/bin/echo -n .
 $cli set repeat.initial_wait 250
 /bin/echo -n .
+$cli set remap.fkeys_to_consumer_f5 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f1 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f10 1
+/bin/echo -n .
+$cli set repeat.wait 15
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f3_lion 1
+/bin/echo -n .
 $cli set remap.doublepresscommandQ 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f7 1
+/bin/echo -n .
+$cli set private.topre.set_option_to_command_and_eisuu_kana 1
+/bin/echo -n .
+$cli set private.topre.set_command_to_option 1
 /bin/echo -n .
 /bin/echo
