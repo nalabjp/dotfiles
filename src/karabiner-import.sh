e3786759df5f88f4ba8281b4cb517df99eaaf663
nalabjp <nalabjp@gmail.com>
2016-06-06 10:06:15 +0900

    Update key remap

--- src/karabiner-import.sh
+++ src/karabiner-import.sh
@@ -6,9 +6,11 @@ $cli set repeat.initial_wait 250
 /bin/echo -n .
 $cli set remap.fkeys_to_consumer_f5 1
 /bin/echo -n .
+$cli set remap.fkeys_to_consumer_f10 1
+/bin/echo -n .
 $cli set remap.fkeys_to_consumer_f1 1
 /bin/echo -n .
-$cli set remap.fkeys_to_consumer_f10 1
+$cli set remap.jis_commandL2commandL_eisuu 1
 /bin/echo -n .
 $cli set repeat.wait 15
 /bin/echo -n .
@@ -16,10 +18,12 @@ $cli set remap.fkeys_to_consumer_f3_lion 1
 /bin/echo -n .
 $cli set remap.doublepresscommandQ 1
 /bin/echo -n .
+$cli set remap.jis_commandR2commandR_kana 1
+/bin/echo -n .
 $cli set remap.fkeys_to_consumer_f7 1
 /bin/echo -n .
-$cli set private.topre.set_option_to_command_and_eisuu_kana 1
-/bin/echo -n .
 $cli set private.topre.set_command_to_option 1
+/bin/echo -n .
+$cli set private.topre.set_option_to_command_and_eisuu_kana 1
 /bin/echo -n .
 /bin/echo
