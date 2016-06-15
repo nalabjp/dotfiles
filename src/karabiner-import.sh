9ac1a2a128cd6401c9f67c4b855fcacadbd37af9
nalabjp <nalabjp@gmail.com>
2016-06-15 16:51:49 +0900

    Enable reverse mouse scroll

--- src/karabiner-import.sh
+++ src/karabiner-import.sh
@@ -8,9 +8,11 @@ $cli set remap.fkeys_to_consumer_f5 1
 /bin/echo -n .
 $cli set remap.fkeys_to_consumer_f10 1
 /bin/echo -n .
+$cli set remap.jis_commandL2commandL_eisuu 1
+/bin/echo -n .
 $cli set remap.fkeys_to_consumer_f1 1
 /bin/echo -n .
-$cli set remap.jis_commandL2commandL_eisuu 1
+$cli set private.remap.reverse_mouse_scroll 1
 /bin/echo -n .
 $cli set repeat.wait 15
 /bin/echo -n .
@@ -22,8 +24,8 @@ $cli set remap.jis_commandR2commandR_kana 1
 /bin/echo -n .
 $cli set remap.fkeys_to_consumer_f7 1
 /bin/echo -n .
-$cli set private.topre.set_command_to_option 1
-/bin/echo -n .
 $cli set private.topre.set_option_to_command_and_eisuu_kana 1
+/bin/echo -n .
+$cli set private.topre.set_command_to_option 1
 /bin/echo -n .
 /bin/echo
